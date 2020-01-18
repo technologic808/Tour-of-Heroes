6cf924e7fc929b90678ae1ced015d867060cb60e
technologic808 <venkys321@gmail.com>
2020-01-18 17:43:20 +0530

    Create separate reusable HeroDetailComponent and use one way property binding to give the parent control over the component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
 import { HeroesComponent } from "./heroes/heroes.component";
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, HeroesComponent],
+  declarations: [AppComponent, HeroesComponent, HeroDetailComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
